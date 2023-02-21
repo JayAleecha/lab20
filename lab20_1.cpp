4c29a2ef9b3b6e8aa4aa8521e21daf13e31fd15f
JayAleecha <aleecha.2546@gmail.com>
2023-02-21 16:27:13 +0700

    lab20_1

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
+double overlap(Rect R1,Rect R2){
+    double under = max(R1.y-R1.h,R2.y-R2.h);
+    double on = min(R1.y,R2.y);
+    double left = max(R1.x,R2.x);
+    double right = min(R1.w+R1.x,R2.w+R2.x);
+    
+    if(on > under && right > left){
+        return (on-under)*(right-left);
+    }else{
+        return 0;
+    }
 
 
 }
